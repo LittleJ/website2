39e2f7d494ed18cfd51f77ccb8b126e72a619b1f
Little J <contact@littlej.fr>
2020-03-07 09:16:42 +0100

    [Home] Integrate the new footer

---
+++ app/Views/ci4-theme/_footer.php
@@ -0,0 +1,26 @@
+<div class="environment">
+
+  <?= $footerbar ?>
+
+  <?= $githubWidget ?>
+
+  <a class="twitter-follow-button"
+     href="https://twitter.com/CodeIgniter"
+     data-show-count="true"
+     data-show-screen-name="false"
+     data-size="large"
+     data-lang="en">
+    @CodeIgniter
+  </a>
+
+</div>
+
+<div class="copyrights">
+
+  <p>&copy; <?= date('Y') ?> CodeIgniter Foundation. CodeIgniter is open source
+     project released under the MIT licence. It has been created by
+     <a href="http://www.ellislab.com/">EllisLab</a>, fostered by
+     <a href="http://www.bcit.ca/cas/computing/">BCIT</a>, and is a project of
+     the CodeIgniter Foundation.</p>
+
+</div>
--- app/Views/ci4-theme/layout.php
+++ app/Views/ci4-theme/layout.php
@@ -1,73 +1,49 @@
 <!doctype html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 
-		<title><?= $pagetitle ?></title>
+    <title><?= $pagetitle ?></title>
 
-		<link rel="stylesheet" type="text/css" href="/assets/css/website.css"/>
-		<link rel="stylesheet" type="text/css" href="/assets/github-buttons/style.css"/>
-	</head>
+    <link rel="stylesheet" type="text/css" href="/assets/css/website.css"/>
+    <link rel="stylesheet" type="text/css" href="/assets/github-buttons/style.css"/>
+  </head>
 
   <body>
 
-<!--
-|--------------------------------------------------------------------------
-| EADER: MENU + HEROE SECTION
-|--------------------------------------------------------------------------
- -->
 
-	<header>
+  <!-- Header: navbar + heroe section ----------------------------------------->
 
-		<?= $navbar ?>
+  <header>
 
-		<?= $this->include('ci4-theme/_heroe') ?>
+    <?= $navbar ?>
 
-	</header>
+    <?= $this->include('ci4-theme/_heroe') ?>
 
-<!--
-|--------------------------------------------------------------------------
-| CONTENT
-|--------------------------------------------------------------------------
- -->
-
-	<?= $this->renderSection('content') ?>
+  </header>
 
 
+  <!-- Content ---------------------------------------------------------------->
 
-		<div class="footer">
-			<div class="footer-menu">
-				<div class="container">
-					<div class="row">
-						<ul class="nav nav-pills">
-							<?= $footerbar ?>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="footer-copyright">
-				<div class="container">
-					<div class="row">
-                        <p class="text-center">CodeIgniter was created by <a href="http://www.ellislab.com/">EllisLab</a>,
-                            fostered by <a href="http://www.bcit.ca/cas/computing/">BCIT</a>,
-                            and is a project of the CodeIgniter Foundation.
-                            <a href="mailto:admin@codeigniter.com"><span class="glyphicon glyphicon-envelope"></span></a></p>
-					</div>
-				</div>
-			</div>
-		</div>
+  <?= $this->renderSection('content') ?>
 
-		<script>!function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
-        if (!d.getElementById(id)) {
-            js = d.createElement(s);
-            js.id = id;
-            js.src = p + '://platform.twitter.com/widgets.js';
-            fjs.parentNode.insertBefore(js, fjs);
-        }
-      }(document, 'script', 'twitter-wjs');
-		</script>
-	</body>
+
+  <!-- Footer ----------------------------------------------------------------->
+
+  <footer>
+
+    <?= $this->include('ci4-theme/_footer') ?>
+
+  </footer>
+
+
+  <!-- JS scripts ------------------------------------------------------------->
+
+  <?= $this->include('ci4-theme/_scripts') ?>
+
+
+  </body>
+
 </html>
